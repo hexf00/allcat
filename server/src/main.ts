bc1c2e647b17b98f67992d1fc3a79b2e588ac9a9
hexf00 <hexf00@gmail.com>
2021-10-19 11:52:11 +0800

    feat:添加前端静态服务

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,8 +6,8 @@ import { join } from 'path';
 console.log("监听 5922 ");
 async function bootstrap () {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  app.useStaticAssets(join(__dirname, '../public/', 'frontend'), {
+  console.log('前端资源目录位于', __dirname)
+  app.useStaticAssets(join(__dirname, 'client'), {
     prefix: '/'
   })
   await app.listen(5922);
